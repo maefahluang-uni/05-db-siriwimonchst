a32490f5bc4330f8a16f1a1c194156d0d977251c
siriwimonchst <6731503125@lamduan.mfu.ac.th>
2025-09-08 09:31:20 +0000

    Owner avatar 05-db-siriwimonchst

--- src/main/java/th/mfu/boot/User.java
+++ src/main/java/th/mfu/boot/User.java
@@ -7,46 +7,55 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.GenerationType;
 
-
+@Entity
 public class User {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String email;
     private String displayname;
     private LocalDate birthday;
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getDisplayname() {
         return displayname;
     }
+
     public void setDisplayname(String displayname) {
         this.displayname = displayname;
     }
+
     public LocalDate getBirthday() {
         return birthday;
     }
+
     public void setBirthday(LocalDate birthday) {
         this.birthday = birthday;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
-    
-    
-    
+
 }
--- src/main/java/th/mfu/boot/UserController.java
+++ src/main/java/th/mfu/boot/UserController.java
@@ -27,29 +27,42 @@ public class UserController {
     public ResponseEntity<String> registerUser(@RequestBody User user) {
 
         //TODO: check if user with the username exists
+        if (user.getUsername() == null || user.getUsername().isBlank()) {
+            return new ResponseEntity<>("username is required", HttpStatus.BAD_REQUEST);
+        }
+        User exists = repo.findByUsername(user.getUsername());
+        if (exists != null) {
+            return new ResponseEntity<>("username already exists", HttpStatus.CONFLICT);
+        }
        
         //TODO: save the user
+        repo.save(user);
 
         //TODO: remove below and return proper status
-        return new ResponseEntity<>( HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/users")
     public ResponseEntity<List<User>> list() {
         
         //TODO: remove below and return proper result
-        return new ResponseEntity<>( HttpStatus.NOT_IMPLEMENTED);
+        List<User> users = repo.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @DeleteMapping("/users/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable Long id) {
         
         //TODO: check if user with the id exists
+        if (!repo.existsById(id)) {
+            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+        }
        
         //TODO: delete the user
+        repo.deleteById(id);
     
         //TODO: remove below and return proper status
-        return new ResponseEntity<>( HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<>("Deleted successfully", HttpStatus.OK);
     }
 
 
